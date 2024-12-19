efe1f8f25ec983e30f2d9d5ab16f4343837fec57
Andrey Kramskih <andreykramskih@gmail.com>
2024-12-19 12:29:18 +0300

    Day12 of streamlit trainning

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pandas as pd
 import numpy as np
 
 
-st.header('st.multiselect')
-value=st.multiselect('What are you favorite colors?', options=['Green', 'Yellow', 'Red', 'Blue'])
-st.write('You selected:',value)
+st.header('st.checkbox')
+st.write('What would you like to order?')
+ice_cream=st.checkbox('Ice cream')
+coffee=st.checkbox('Coffee')
+cola=st.checkbox("Cola")
+
+if ice_cream:
+    st.write("Great! Here's some more 🍦")
+if coffee:
+    st.write("Okay, here's some coffee ☕")
+if cola:
+    st.write("Here you go 🥤")
