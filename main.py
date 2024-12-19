0a55e68a423a3b199dd6873b8de0b8f76b9824d9
Andrey Kramskih <andreykramskih@gmail.com>
2024-12-19 12:11:58 +0300

    Day10 of streamlit trainning

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import pandas as pd
 import numpy as np
 
 
-st.header('Line chart')
-chart=pd.DataFrame(
-    np.random.randn(20,4),
-    columns=['a', 'b', 'c', 'd']
-)
-
-st.line_chart(chart)
+st.header('Select box')
+check=st.selectbox('What is your favorite color?', options=['Red', 'Blue', 'Green'])
+st.write('Your favorite color is', check)
