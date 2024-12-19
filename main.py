6a42c77287cf650cb0147577880196e07af7263c
Andrey Kramskih <andreykramskih@gmail.com>
2024-12-19 11:27:56 +0300

    Day8 of streamlit trainning

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-from random import randint
-
-import pandas as pd
-import streamlit as st
-import numpy as np
-import time
-import altair as alt
-
 import streamlit as st
 
-st.header('st.write')
-st.subheader('Text input')
-st.write('*Hello*, *World!* :sunglasses:')
-st.subheader('Display numbers')
-st.write(1234)
-st.subheader('Display DataFrame')
-data=pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10,20,30,40]
-})
-st.write(data)
-#st.subheader('Accept multiple arguments')
-st.write('Below is DataFrame:')
-st.write(data)
-st.write('Above is dataframe:')
-st.subheader('Display Charts')
+from datetime import date, time, datetime
 
-data_chart=pd.DataFrame(
-    np.random.randn(200,3),
-    columns=['a', 'b', 'c']
+st.header('st.slider')
+age=st.slider('How old are you?', 0,130,25)
+st.write('I am',age,'years old')
+
+st.header('Range slider')
+select=st.slider('Select a range of values',0,100,[25,75])
+st.write('Values:',select)
+
+st.header('Range time slider')
+timerange=st.slider('Shedule your appointment',value=(time(11,30), time(12, 45)))
+st.write('You`re sheduled for:',timerange)
+
+st.header('Datetime slider')
+daterange=st.slider(
+    'When do you start?',
+    value=datetime(2020,1, 1, 9, 30 ),
+    format='MM/DD/YY - hh:mm'
 )
-chart=alt.Chart(data_chart).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
-)
-st.write(chart)
+st.write('Start time:',daterange)
